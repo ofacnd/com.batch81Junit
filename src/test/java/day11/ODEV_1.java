534b4cb22386ba96b5f34cf19f90ba592b180a20
Omer Fatih ORUCU <omerfatihorucu@gmail.com>
2022-09-22 02:44:12 -0600

    Actions, FileExist, Faker

--- src/test/java/day11/ODEV_1.java
+++ src/test/java/day11/ODEV_1.java
@@ -1,19 +1,29 @@
 package day11;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import utilities.TestBaseBeforeAfter;
 
 public class ODEV_1 extends TestBaseBeforeAfter {
     @Test
-    public void test01() {
+    public void test01() throws InterruptedException {
 //1- "http://webdriveruniversity.com/Actions" sayfasina gidin
         driver.get("http://webdriveruniversity.com/Actions");
         //   2- Hover over Me First" kutusunun ustune gelin
+        actions.moveToElement(driver.findElement(By.xpath("(//*[@class='dropbtn'])[1]"))).perform();
+        Thread.sleep(2000);
         //   3- Link 1" e tiklayin
+        driver.findElement(By.xpath("(//*[text()='Link 1'])[1]")).click();
         //   4- Popup mesajini yazdirin
+        System.out.println("Pop-up Mesaji ....> " + driver.switchTo().alert().getText());
         //   5- Popup'i tamam diyerek kapatin
+        Thread.sleep(1500);
+        driver.switchTo().alert().accept();
         //   6- "Click and hold" kutusuna basili tutun
+        actions.clickAndHold(driver.findElement(By.xpath("//*[@id='click-box']"))).perform();
         //   7-"Click and hold" kutusunda cikan yaziyi yazdirin
+        System.out.println("Click And Hold Box`ta Yazan Yazi....> " + driver.findElement(By.xpath("//*[@id='click-box']")).getText());
         //   8- "Double click me" butonunu cift tiklayin
+        actions.doubleClick(driver.findElement(By.xpath("//*[text()='Double Click Me!']"))).perform();
     }
 }
--- src/test/java/day11/ODEV_2.java
+++ src/test/java/day11/ODEV_2.java
@@ -1,14 +1,34 @@
 package day11;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
 import utilities.TestBaseBeforeAfter;
 
 public class ODEV_2 extends TestBaseBeforeAfter {
     @Test
-    public void odev2(){
-        //Bir Class olusturalim KeyboardActions2
-        //https://html.com/tags/iframe/ sayfasina gidelim  3- video’yu gorecek kadar asagi inin
+    public void odev2() throws InterruptedException {
+        //https://html.com/tags/iframe/ sayfasina gidelim
+        driver.get("https://html.com/tags/iframe/");
+
+        // 3- video’yu gorecek kadar asagi inin
+        // WebElement video = driver.findElement(By.xpath("//*[@class='ytp-cued-thumbnail-overlay-image']"));
+        actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
+
         //videoyu izlemek icin Play tusuna basin
+        WebElement video = driver.findElement(By.xpath("//*[@src='https://www.youtube.com/embed/owsfdh4gxyc']"));
+        driver.switchTo().frame(video);
+        driver.findElement(By.xpath("//*[@aria-label='Play']")).click();
+
         //videoyu calistirdiginizi test edin
+      //  driver.switchTo().defaultContent();
+      //  Thread.sleep(1000);
+      //  actions.moveToElement(driver.findElement(By.xpath("//*[@class='video-stream html5-main-video']"))).perform();
+      //  Thread.sleep(1500);
+//
+      //  Assert.assertFalse(driver.findElement(By.xpath("//*[@title='Pause (k)']")).isDisplayed());
     }
 }
