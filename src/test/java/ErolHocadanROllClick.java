04c8a44fe8ad8cf1c69f1375e1514588caf09cf1
Omer Fatih ORUCU <omerfatihorucu@gmail.com>
2022-09-28 21:19:54 -0600

    ScreenShot

---
+++ src/test/java/ErolHocadanROllClick.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import utilities.TestBaseBeforeAfter;
+
+
+public class ErolHocadanROllClick {
+    @Test
+  public void erolHocadanTrick() {
+  //    JavascriptExecutor js = null;
+  //    Object auto = null;
+  //    js.executeScript("arguments[0].scrollIntoView(true);", auto.downloadInvoiceButton);
+  //    try {
+  //        driver.findElement(By.linkText("Scans")).click();
+  //    } catch (Exception e) {
+  //        JavascriptExecutor executor = (JavascriptExecutor) driver;
+  //        executor.executeScript("arguments[0].click();", driver.findElement(By.linkText("Scans")));
+     //   }
+    }
+}
